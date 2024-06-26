f2176e7a6733017853fd5cc4502c93524f53c851
Nikita Yuzhakov <yuzhakov.nikita@gmail.com>
2024-06-25 20:11:27 -0400

    feat: Create DataTable component; Fetch users from API and show in DataTable

--- src/routes/home/Home.tsx
+++ src/routes/home/Home.tsx
@@ -1,7 +1,86 @@
+import { useEffect, useState } from "react";
+import { Box } from "@mui/material";
+import DataTable, { HeadCell } from "./components/DataTable";
+
+type User = {
+  id: number,
+  name: string;
+  username: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: string;
+      lng: string;
+    }
+  }
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  }
+};
+
+const headCells: HeadCell[] = [
+  {
+    id: 'id',
+    numeric: true,
+    label: 'ID',
+  },
+  {
+    id: 'name',
+    numeric: false,
+    label: 'Name',
+  },
+  {
+    id: 'email',
+    numeric: false,
+    label: 'Email',
+  },
+  {
+    id: 'companyName',
+    numeric: false,
+    label: 'Company Name',
+  },
+];
+
 function Home() {
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      let users;
+      try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users');
+        users = await response.json();
+      } catch (error) {
+        console.error(error);
+      }
+
+      setUsers(users)
+    }
+
+    fetchUsers();
+  }, []);
+
   return (
     <div>
-      Home
+      <Box sx={{ m:  { xs: 0, md: 4 } }}>
+        <DataTable
+          headCells={headCells}
+          rows={users.map(user => ({
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            companyName: user.company.name
+          }))}
+        />
+      </Box>
     </div>
   );
 }
---
+++ src/routes/home/components/DataTable.tsx
@@ -0,0 +1,185 @@
+import { useMemo, useState } from 'react';
+import Box from '@mui/material/Box';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
+import TableSortLabel from '@mui/material/TableSortLabel';
+import Paper from '@mui/material/Paper';
+import { visuallyHidden } from '@mui/utils';
+
+function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+type Order = 'asc' | 'desc';
+
+function getComparator<Key extends keyof any>(
+  order: Order,
+  orderBy: Key,
+): (
+  a: { [key in Key]: number | string },
+  b: { [key in Key]: number | string },
+) => number {
+  return order === 'desc'
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+export interface HeadCell {
+  id: string;
+  label: string;
+  numeric: boolean;
+}
+
+interface DataTableHeadProps {
+  headCells: HeadCell[];
+  onRequestSort: (event: React.MouseEvent<unknown>, property: string) => void;
+  order: Order;
+  orderBy: string;
+  rowCount: number;
+}
+
+function DataTableHead(props: DataTableHeadProps) {
+  const { order, orderBy, onRequestSort } =
+    props;
+  const createSortHandler =
+    (property: string) => (event: React.MouseEvent<unknown>) => {
+      onRequestSort(event, property);
+    };
+
+  return (
+    <TableHead>
+      <TableRow>
+        {props.headCells.map((headCell) => (
+          <TableCell
+            key={headCell.id}
+            align="left"
+            sortDirection={orderBy === headCell.id ? order : false}
+          >
+            <TableSortLabel
+              active={orderBy === headCell.id}
+              direction={orderBy === headCell.id ? order : 'asc'}
+              onClick={createSortHandler(headCell.id)}
+            >
+              {headCell.label}
+              {orderBy === headCell.id ? (
+                <Box component="span" sx={visuallyHidden}>
+                  {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                </Box>
+              ) : null}
+            </TableSortLabel>
+          </TableCell>
+        ))}
+      </TableRow>
+    </TableHead>
+  );
+}
+
+interface DataTableProps {
+  headCells: HeadCell[];
+  rows: any[];
+}
+
+export default function DataTable({ headCells, rows }: DataTableProps) {
+  const [order, setOrder] = useState<Order>('asc');
+  const [orderBy, setOrderBy] = useState<string>(headCells[0].id);
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+
+  const handleRequestSort = (
+    event: React.MouseEvent<unknown>,
+    property: string,
+  ) => {
+    const isAsc = orderBy === property && order === 'asc';
+    setOrder(isAsc ? 'desc' : 'asc');
+    setOrderBy(property);
+  };
+
+  const handleChangePage = (event: unknown, newPage: number) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  // NOTE: Avoid a layout jump when reaching the last page with empty rows.
+  const emptyRows =
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
+
+  const visibleRows = useMemo(
+    () =>
+      rows.slice().sort(getComparator(order, orderBy)).slice(
+        page * rowsPerPage,
+        page * rowsPerPage + rowsPerPage,
+      ),
+    [rows, order, orderBy, page, rowsPerPage],
+  );
+
+  return (
+    <Box sx={{ width: '100%' }}>
+      <Paper sx={{ width: '100%', mb: 2 }}>
+        <TableContainer>
+          <Table
+            sx={{ minWidth: 750 }}
+            aria-labelledby="tableTitle"
+            size="small"
+          >
+            <DataTableHead
+              headCells={headCells}
+              order={order}
+              orderBy={orderBy}
+              onRequestSort={handleRequestSort}
+              rowCount={rows.length}
+            />
+            <TableBody>
+              {visibleRows.map((row) => {
+                return (
+                  <TableRow
+                    tabIndex={-1}
+                    key={row.id}
+                  >
+                    {headCells.map((headCell) => (
+                      <TableCell key={headCell.id} align="left">
+                        {row[headCell.id]}
+                      </TableCell>
+                    ))}
+                  </TableRow>
+                );
+              })}
+              {emptyRows > 0 && (
+                <TableRow
+                  style={{
+                    height: 33
+                  }}
+                >
+                  <TableCell colSpan={6} />
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[5, 10, 25]}
+          component="div"
+          count={rows.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+      </Paper>
+    </Box>
+  );
+}

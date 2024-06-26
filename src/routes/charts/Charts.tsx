3bdbe348ab408d96bdfb756d9ca2533c680acaf5
Nikita Yuzhakov <yuzhakov.nikita@gmail.com>
2024-06-26 19:57:21 -0400

    feat: Install chart.js and react-chartjs-2 packages; Create Pie Chart that shows users amount by name first letter

--- src/routes/charts/Charts.tsx
+++ src/routes/charts/Charts.tsx
@@ -1,8 +1,70 @@
+import { useContext, useMemo } from 'react';
+import { Box, Typography } from '@mui/material';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend, Colors } from 'chart.js';
+import { Pie } from 'react-chartjs-2';
+
+import UsersContext from '../../contexts/UsersContext';
+
+ChartJS.register(ArcElement, Tooltip, Legend, Colors);
+
 function Charts() {
+  const { users } = useContext(UsersContext);
+
+  const data = useMemo(() => {
+    const usersAmountByNameFirstLetter: { [key: string]: number } = {};
+
+    for (const user of users) {
+      const nameFirstLetter = user.name[0].toUpperCase();
+      const usersAmount = usersAmountByNameFirstLetter[nameFirstLetter] || 0;
+
+      usersAmountByNameFirstLetter[nameFirstLetter] = usersAmount + 1;
+    }
+
+    const data = {
+      labels: [],
+      datasets: [
+        {
+          label: '# of Users',
+          data: [],
+          borderWidth: 1,
+        },
+      ],
+    };
+
+    for (const nameFirtLetter in usersAmountByNameFirstLetter) {
+      // @ts-ignore
+      data.labels.push(nameFirtLetter);
+      // @ts-ignore
+      data.datasets[0].data.push(usersAmountByNameFirstLetter[nameFirtLetter])
+    }
+
+    return data;
+  }, [users]);
+
   return (
-    <div>
-      Charts
-    </div>
+    <Box sx={{
+      minHeight: '70vh',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center'
+    }}>
+      <Box sx={{ mb: 4 }}>
+        <Typography>Users Amount By Name First Letter</Typography>
+      </Box>
+      
+      <Box sx={{ maxHeight: 300, maxWidth: 300 }}>
+        {data.datasets[0].data.length > 0 && (
+          <Pie data={data} options={{
+            plugins: {
+              legend: {
+                position: 'bottom'
+              },
+            }
+          }} />
+        )}
+      </Box>
+    </Box>
   );
 }
 
